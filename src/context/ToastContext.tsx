369a59c83b85ef96d01bf388a4dc30310669445a
Parvesh <arveshchaudhary@gmail.com>
2026-02-08 19:32:18 +0545

    Toast Context and wrap in Main

---
+++ src/context/ToastContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useState } from "react";
+
+interface ToastContextType {
+  showToast: (message: string) => void;
+}
+
+const ToastContext = createContext<ToastContextType | null>(null);
+
+export const ToastProvider = ({ children }: { children: React.ReactNode }) => {
+    const [toast, setToast] = useState<{ message: string; visible: boolean }>({ message: "", visible: false })
+
+    //ShowToast
+    const showToast = (message: string) => {
+        setToast({ message, visible: true });
+
+        setTimeout(() => {
+            setToast({ message: "", visible: false });
+        }, 2500); 
+    };
+
+    return (
+        <ToastContext.Provider value={{ showToast }}>
+            {children}
+            {/* Toast Popup */}
+            {toast.visible && (
+                <div className="fixed bottom-6 right-6 bg-yellow-500 text-black px-5 py-3 rounded-lg shadow-lg animate-fadeInUp z-50">
+                    {toast.message}
+                </div>
+            )}
+        </ToastContext.Provider>
+    )
+
+};
+
+export const useToast = () => {
+  const context = useContext(ToastContext);
+  if (!context) throw new Error("useToast must be used within ToastProvider");
+  return context;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import App from './App.tsx'
 import { DataProvider } from './context/DataContext.tsx'
 import { CartProvider } from './context/CartContext.tsx'
 import { FavoriteProvider } from './context/FavoriteContext.tsx'
+import { ToastProvider } from './context/ToastContext.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <DataProvider>
     <CartProvider>
       <FavoriteProvider>
-        <App />
+        <ToastProvider>
+          <App />
+        </ToastProvider>
       </FavoriteProvider>
     </CartProvider>
   </DataProvider>,
